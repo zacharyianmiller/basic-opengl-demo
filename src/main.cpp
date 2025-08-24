f5f086c7e5e0b16002f060b6adbce5c14d3dc8c4
Zachary Miller <zacharyianmiller1@gmail.com>
2025-08-24 16:21:11 -0500

    glew added

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,84 @@
+#ifdef __APPLE__
+#define GL_SILENCE_DEPRECATION 1
+#endif
+
+#include <OpenGL/OpenGL.h>
+#include <OpenGL/gl3.h>
+
 #include "../ext/glfw/include/GLFW/glfw3.h"
+
+#include <string>
+#include <iostream>
+
+static unsigned int compileShader (unsigned int type, const std::string& source)
+{
+    unsigned int id = glCreateShader(type);
+    const char* src = source.c_str();
+    glShaderSource(id, 1, &src, nullptr);
+    glCompileShader(id);
+
+    // Error handling
+    int result;
+    glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+    if (result == GL_FALSE)
+    {
+        int length;
+        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+        char* message = (char*)alloca(length * sizeof(char));
+        glGetShaderInfoLog(id, length, &length, message);
+
+        const char* shaderType;
+        if (type == GL_VERTEX_SHADER) {
+            shaderType = "vertex";
+        } else if (type == GL_FRAGMENT_SHADER) {
+            shaderType = "fragment";
+        } else {
+            shaderType = "undefined";
+        }
+
+        std::cout << "Failed to compile " << shaderType << " shader!" << std::endl;
+        std::cout << message << std::endl;
+
+        glDeleteShader(id);
+        return 0;
+    }
+
+    return id;
+}
+
+static unsigned int createShader (const std::string& vertexShader, const std::string& fragmentShader)
+{
+    unsigned int program = glCreateProgram();
+    unsigned int vs = compileShader(GL_VERTEX_SHADER, vertexShader);
+    unsigned int fs = compileShader(GL_FRAGMENT_SHADER, fragmentShader);
+
+    glAttachShader(program, vs);
+    glAttachShader(program, fs);
+    glLinkProgram(program);
+    glValidateProgram(program);
+
+    // Can delete because already link to program
+    glDetachShader(program, vs);
+    glDetachShader(program, fs);
+
+    return program;
+}
 
 int main(void)
 {
     GLFWwindow* window;
 
-    /* Initialize the library */
+    /* Initialize GLFW */
     if (!glfwInit())
         return -1;
+
+    /* Force GLFW3 to enable v310 shaders */
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    #ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    #endif
 
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(800, 800, "basic-opengl-demo", NULL, NULL);
@@ -19,11 +91,55 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+    float positions[6] {
+        -0.5f, -0.5f,
+         0.5f, -0.5f,
+         0.0f,  0.5f
+    };
+
+    // Shader
+    unsigned int vao, vbo;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+
+    // Shape
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, (void*)0);
+
+    std::string vertexShader = 
+        "#version 330 core\n" // glsl (w/ no deprecated functions)
+        "\n"
+        "layout(location = 0) in vec4 position;" // should match 1st val of glVertexAttribPointer 
+        "\n"
+        "void main()\n"
+        "{\n"
+        "   gl_Position = position;\n"
+        "}\n\0";
+
+    std::string fragmentShader = 
+        "#version 330 core\n"
+        "\n"
+        "layout(location = 0) out vec4 color;"
+        "\n"
+        "void main()\n"
+        "{\n"
+        "   color = vec4(1.f, 0.f, 0.f, 1.f);\n"
+        "}\n\0";
+
+    unsigned int shader = createShader(vertexShader, fragmentShader);
+    glUseProgram(shader);
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
